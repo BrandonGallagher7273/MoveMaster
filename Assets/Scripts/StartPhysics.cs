a55021fcc689d58495292d3dca4921ae7f74fdb6
KAALMOMA <katrinamosquera@gmail.com>
2024-07-27 16:50:32 -0400

    Consolidated Start/Stop Buttons, Deleted Unused Scripts

--- Assets/Scripts/StartPhysics.cs
+++ Assets/Scripts/StartPhysics.cs
@@ -6,20 +6,43 @@ using UnityEngine;
 public class StartPhysics : MonoBehaviour
 {
     public MeshRenderer rend;
+    public Renderer matRend;
+    public Material start_mat;
+    public Material stop_mat;
+    public GameObject player;
+    public bool clicked = false;
     void Start() {
         rend = GetComponent<MeshRenderer>();
+        matRend = GetComponent<Renderer>(); 
+        player = GameObject.Find("Player");
     }
     void Update() {
         if (DestroyOnClick.started == false) {
-            rend.enabled=true;
-            transform.position = new Vector3(47f, 1f, 44f);
+            matRend.material = start_mat;
+            //transform.position = new Vector3(47f, 1f, 44f);
         } else {
-            rend.enabled=false;
-            transform.position = new Vector3(40.5f, 1f, 42f);
+            matRend.material = stop_mat;
+            //transform.position = new Vector3(40.5f, 1f, 42f);
         }
     }
     private void OnMouseDown()
     {
         DestroyOnClick.started = true;
+        matRend.material = stop_mat;
+        if(clicked)
+        {
+            matRend.material = start_mat;
+            Debug.Log("cube works2\n");
+            DestroyOnClick.started = false;
+            BlocksCopy bc_1 = player.GetComponent<BlocksCopy>();
+            bc_1.SpawnBlocks();
+            BlocksCopy_4 bc_2 = player.GetComponent<BlocksCopy_4>();
+            bc_2.SpawnBlocks();
+            clicked = false;
+        }
+        else
+        {
+            clicked = true;
+        }
     }
 }
